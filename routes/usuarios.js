086acc07c9d5d3ea25d24b9ad8c57d0f2672ce89
Inaki-Gongora <i.gongora@alumno.etec.um.edu.ar>
2025-04-12 22:12:10 -0300

    bcryptjs por bcrypt

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Cambiado de bcrypt a bcryptjs
 const jwt = require('jsonwebtoken');
 const secretKey = 'mondongo';
 const authMiddleware = require('../middleware/auth');
